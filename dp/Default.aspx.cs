41268cacb7676679c7a2922ab56ab9cb9c1e47d3
Zhang <jacob.zhang@connext.com.cn>
2020-03-20 10:35:56 +0800

    first commit

---
+++ dp/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write( Get());
+    }
+
+    private String Get()
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connectionStr"].ConnectionString);
+        connection.Open();
+        String sql = "insert test(id)values(1)";
+        SqlCommand command = new SqlCommand(sql, connection);
+        int a = command.ExecuteNonQuery();
+        sql = "select count(0) from test";
+        command = new SqlCommand(sql, connection);
+        object obj = command.ExecuteScalar();
+        return obj.ToString();
+    }
+}
